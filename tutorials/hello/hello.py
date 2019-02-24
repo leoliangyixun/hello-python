3be50bafeba3d26aeefae8e48c49b5771f040636
yangkai <yangkai@hujiang.com>
2019-02-24 16:07:47 +0800

    init

---
+++ tutorials/hello/hello.py
@@ -0,0 +1,39 @@
+# condition = 1
+# while condition < 10:
+#     print(condition)
+#     condition = condition + 1
+
+# list = [1,2,3,4,5,6,7,8,9,10]
+# for i in list:
+#     print(i)
+#     print(i)
+#
+
+# for i in range(1,10,2):
+#     print(i)
+
+x = 4
+y = 2
+z = 3
+if x > 1:
+    print('x > 1')
+elif x < 1:
+    print('x < 1')
+elif x == 1:
+    print('x == 1')
+else:
+    print('end')
+
+
+#Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+#
+# class Solution:
+#     def levelOrder(self, root: TreeNode) -> List[List[int]]:
+
+
+
